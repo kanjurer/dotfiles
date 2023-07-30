524dcd3c8e2a85f0be0d0e1d59d9d707f844c078
kanjurer <kanavb9999@gmail.com>
2023-07-30 00:25:55 -0300

    Created installation script for git dot files

---
+++ git/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# author: Kanav Bhardwaj
+
+# It installs the git config files by creating symlinks at user level
+# Takes an optional parameter to specify the destination of install
+
+shopt -s dotglob
+for filename in *.gitconfig; do
+    src="${PWD}/${filename}"
+    dest=${1:-~/}
+    ln -s ${src} ${dest}
+    echo "Copying ${filename}"
+done
+
+echo "Installation completed!"
